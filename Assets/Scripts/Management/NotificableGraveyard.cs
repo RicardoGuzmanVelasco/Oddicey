c838258fd54399f558c67f159c4c6fac29e98d5e
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2018-11-17 12:42:41 +0100

    #13 | Base para el cementerio

---
+++ Assets/Scripts/Management/NotificableGraveyard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificableGraveyard : MonoBehaviour
+{
+    HashSet<Notificable> undeads;
+
+    void Awake()
+    {
+        undeads = new HashSet<Notificable>();
+    }
+
+    public void Add(Notificable notificable)
+    {
+        if(!undeads.Add(notificable))
+            Debug.LogError(notificable + " have already subscribed into the graveyard");
+    }
+
+    void Revive()
+    {
+        foreach(var undead in undeads)
+            undead.enabled = true;
+        undeads.Clear();
+    }
+
+    /// <summary>
+    /// Discards all current <see cref="Notificable"/> awaiting for reactivation.
+    /// </summary>
+    /// <example>
+    /// When die reaches a new persistent checkpoint, all past <see cref="undeads"/> will never be revived.
+    /// </example>
+    public void Purgue()
+    {
+        //TODO: if necessary, destroy all Notificable components to optimization purposes.
+        undeads.Clear();
+    }
+}
