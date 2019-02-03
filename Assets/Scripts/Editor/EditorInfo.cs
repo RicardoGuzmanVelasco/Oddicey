7ebc744e751036410d5100d1af9eca8a81906bdc
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2019-02-03 21:04:49 +0100

    #221 | Comprobadores de escena unitarios y uno que ejecuta todos
    Centralizada además la información relativa al editor. Soporte a:
        Etiquetas.
        Capas.
        Capas de orden.
        Taxonomía de menús.
        Atajos.
        Prioridad de menús.
    Faltan por completar las no relativas al menú.

---
+++ Assets/Scripts/Editor/EditorInfo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils.Extensions;
+
+namespace Utils.EditorExtensions
+{
+    public static class Tags
+    {
+
+    }
+
+    public static class SortingLayers
+    {
+
+    }
+
+    public static class Layers
+    {
+
+    }
+
+    public static class MenuItems
+    {
+        /// <summary>
+        /// Base multiplier to add a separator between menu items.
+        /// </summary>
+        /// <remarks>
+        /// To pin upper any menu item, just multiply this const by the required priority (...3 > 2 > 1).
+        /// </remarks>
+        public const int Priority = 50;
+
+        public const string Tools = "Tools/";
+            public const string SceneChecker = Tools + "Scene checks/";
+                public const string CheckAll = SceneChecker + "Check all";
+                public const string FindFloatingActors = SceneChecker + "Find floating actors";
+                public const string FindOverlappingObstacles = SceneChecker + "Find overlapping obstacles";
+        #region Hotkeys
+        public const string HotkeyCtrl = "%";
+        public const string HotkeyShift = "#";
+        public const string HotkeyAlt = "&";
+        public const string HotkeyNoCommands = "_";
+        public const string HotkeyCtrlShift = HotkeyCtrl + HotkeyShift;
+        public const string HotkeyCtrlAlt = HotkeyCtrl + HotkeyAlt;
+        public const string HotkeyCtrlShiftAlt = HotkeyCtrl + HotkeyShift + HotkeyAlt;
+
+        /// <summary>
+        /// Hotkey character combinations are added to the end of the menu item path, preceded by a space.
+        /// This function is only for mnemonic purposes. It couldn't be used as consts can't use methods to be defined.
+        /// </summary>
+        /// <remarks>
+        /// <paramref name="hotkeys"/> must contain only elements declared in this class as Hotkey- consts.
+        /// </remarks>
+        /// <param name="key"> Character representing the keyboard key to attach the hotkey.</param>
+        /// <param name="hotkeys">Optional sequence of commands to attach the hotkey.</param>
+        /// <seealso cref="https://unity3d.com/es/learn/tutorials/topics/interface-essentials/unity-editor-extensions-menu-items."/>
+        public static string Hotkey(char key, params string[] hotkeys)
+        {
+            string commands = " ";
+            foreach(var command in hotkeys)
+                commands += command;
+
+            return (commands.HasContent() ? commands : "_") + key; 
+        }
+        #endregion
+    }
+}
--- Assets/Scripts/Editor/SceneChecker.cs
+++ Assets/Scripts/Editor/SceneChecker.cs
@@ -1,62 +1,105 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Utils.Extensions;
+using Utils.StaticExtensions;
 
-enum CheckerMsg
+namespace Utils.EditorExtensions
 {
-    Starting,
-    Error,
-    Finished
-}
-
-public class SceneChecker
-{
-    [MenuItem("Tools/Scene Checks/Find floating actors %#&f")]
-    public static void FindFloatingActors()
+    enum CheckerMsg
     {
-        LogCheck(CheckerMsg.Starting, "Find floating actors in scene");
-
-        HashSet<GameObject> actors = new HashSet<GameObject>();
-        HashSet<Vector2> floors = new HashSet<Vector2>();
-
-        //Get all applicable actors in scene (builders and die atm).
-        foreach(var builder in GameObject.FindObjectsOfType<Builder>())
-            if(!IsPlatform(builder))
-                actors.Add(builder.gameObject);
-        actors.Add(GameObject.FindObjectOfType<Die>().gameObject);
-
-        //Get all floor positions.
-        foreach(var gameObject in GameObject.FindGameObjectsWithTag("Floor"))
-            floors.Add(gameObject.transform.position);
-
-        //Check if all applicable actors has any floor just below.
-        foreach(var actor in actors)
-            if(!floors.Contains(actor.transform.position))
-                LogCheck(CheckerMsg.Error, "", "Actor " + actor + " is floating in scene.");
-
-        LogCheck(CheckerMsg.Finished, "Find floating actors in scene");
+        Starting,
+        Error,
+        Warning,
+        Finished
     }
 
-    static void LogCheck(CheckerMsg type, string checkTest, string msg = "")
+    public class SceneChecker
     {
-        msg = "[" + checkTest + "] " + msg;
-        switch(type)
+        /// <summary>
+        /// Check all tests hereinafter.
+        /// </summary>
+        /// <hotkey>CTRL+SHIFT+ALT+A</hotkey>
+        [MenuItem(MenuItems.CheckAll + " " + MenuItems.HotkeyCtrlShiftAlt + "a", false, MenuItems.Priority)]
+        public static void CheckAll()
         {
-            case CheckerMsg.Error:
-                Debug.LogError(msg);
-                break;
-            case CheckerMsg.Starting:
-            case CheckerMsg.Finished:
-                Debug.Log(msg + type.ToString() + ".");
-                break;
-            default:
-                break;
+            LogCheck(CheckerMsg.Starting, "ALL CHECKINGS ARE RUNNING");
+            FindFloatingActors();
+            FindOverlappingObstacles();
+            LogCheck(CheckerMsg.Finished, "ALL CHECKINGS FINISHED");
+        }
+
+
+        /// <summary>
+        /// Check all the applicable actors are on the ground (it is, a platform), not floating in the void.
+        /// </summary>
+        /// <hotkey>CTRL+SHIFT+ALT+F</hotkey>
+        [MenuItem(MenuItems.FindFloatingActors + " " + MenuItems.HotkeyCtrlShiftAlt + "f")]
+        public static void FindFloatingActors()
+        {
+            LogCheck(CheckerMsg.Starting, "Find floating actors in scene");
+
+            HashSet<GameObject> actors = new HashSet<GameObject>();
+            HashSet<Vector2> floors = new HashSet<Vector2>();
+
+            //Get all applicable actors in scene (no platforms, just builders and die).
+            foreach(var builder in GameObject.FindObjectsOfType<Builder>())
+                if(!builder.GetComponentInParent<PlatformBuilder>()) //Not applicable if platform.
+                    actors.Add(builder.gameObject);
+            actors.Add(GameObject.FindObjectOfType<Die>().gameObject);
+
+            //Get all floor positions.
+            foreach(var gameObject in GameObject.FindGameObjectsWithTag("Floor"))
+                floors.Add(gameObject.transform.position);
+
+            //Check if all applicable actors has any floor just below.
+            foreach(var actor in actors)
+                if(!floors.Contains(actor.transform.position))
+                    LogCheck(CheckerMsg.Error, "Actor " + actor.name + " is floating in scene");
+
+            LogCheck(CheckerMsg.Finished, "Find floating actors in scene");
+        }
+
+        /// <summary>
+        /// Check all squares are populated by a single applicable actor at once.
+        /// </summary>
+        /// <hotkey>CTRL+SHIFT+ALT+O</hotkey>
+        [MenuItem(MenuItems.FindOverlappingObstacles + " " + MenuItems.HotkeyCtrlShiftAlt + "o")]
+        public static void FindOverlappingObstacles()
+        {
+            LogCheck(CheckerMsg.Starting, "Find overlapping obstacles in scene");
+
+            HashSet<Vector2> actors = new HashSet<Vector2>();
+
+            //Get all positions of applicable builders in scene (neither platforms nor marks).
+            foreach(var builder in GameObject.FindObjectsOfType<Builder>())
+                if(!builder.GetComponent<MarkBuilder>() && !builder.GetComponentInParent<PlatformBuilder>())
+                    if(!actors.Add(builder.transform.position)) //If set already owned this position, an overlapping exists.
+                        LogCheck(CheckerMsg.Error, "Overlap exists in " + (Vector2)builder.transform.position);
+
+            LogCheck(CheckerMsg.Finished, "Find overlapping obstacles in scene");
+        }
+
+        #region Log
+        static void LogCheck(CheckerMsg type, string msg)
+        {
+            msg += ".";
+            switch(type)
+            {
+                case CheckerMsg.Warning:
+                    Debug.LogWarning(msg);
+                    break;
+                case CheckerMsg.Error:
+                    Debug.LogError(msg);
+                    break;
+                case CheckerMsg.Starting:
+                case CheckerMsg.Finished:
+                    Debug.Log(type.ToString() + ": " + msg);
+                    break;
+                default:
+                    break;
+            }
         }
     }
-
-    static bool IsPlatform(Builder builder)
-    {
-        return builder.GetComponentInParent<PlatformBuilder>();
-    }
-
+    #endregion
 }
