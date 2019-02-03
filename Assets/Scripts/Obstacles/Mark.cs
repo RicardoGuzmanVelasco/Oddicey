417223a8327385fa626e0ff33a9d6783538d52a0
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2019-02-03 13:11:24 +0100

    #223 | Huella no respeta lado prefijado tras muerte
    Eliminada comprobación innecesaria de si la huella es aleatoria antes de llamar al método.
        Venía heredado del código anterior en que no se comprobaba desde dentro.

--- Assets/Scripts/Obstacles/Mark.cs
+++ Assets/Scripts/Obstacles/Mark.cs
@@ -13,83 +13,82 @@ public class Mark : Notificable
     /// </summary>
     [HideInInspector]
     public bool randomSide;
-	/// <summary>
-	/// Side that will switch on the obstacle.
-	/// </summary>
+    /// <summary>
+    /// Side that will switch on the obstacle.
+    /// </summary>
     [HideInInspector]
-	public int sideRequired;
-	/// <summary>
-	/// Whether or not switched on (OK or WRONG state).
-	/// </summary>
-	private bool state;
+    public int sideRequired;
+    /// <summary>
+    /// Whether or not switched on (OK or WRONG state).
+    /// </summary>
+    private bool state;
 
-	/// <summary>
-	/// Player's die reference.
-	/// </summary>
+    /// <summary>
+    /// Player's die reference.
+    /// </summary>
     [HideInInspector]
-	public Die player;
+    public Die player;
 
-	/// <summary>
-	/// Sprites list for each side of the die.
-	/// </summary>
+    /// <summary>
+    /// Sprites list for each side of the die.
+    /// </summary>
     [HideInInspector]
-	public Sprite[] sprites;
-	/// <summary>
-	/// Unique renderer of the current side.
-	/// </summary>
-	protected SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    /// <summary>
+    /// Unique renderer of the current side.
+    /// </summary>
+    protected SpriteRenderer spriteRenderer;
 
-	#region Properties
-	/// <value>
-	/// This value change too the mark skin!
-	/// </value>
-	public bool State
-	{
-		get
-		{
-			return state;
-		}
+    #region Properties
+    /// <value>
+    /// This value change too the mark skin!
+    /// </value>
+    public bool State
+    {
+        get
+        {
+            return state;
+        }
 
-		set
-		{
-			state = value;
-			//TODO: will be changed by animation play. That animation will make the color change.
-			spriteRenderer.color = state ? Color.green : Color.red;
+        set
+        {
+            state = value;
+            //TODO: will be changed by animation play. That animation will make the color change.
+            spriteRenderer.color = state ? Color.green : Color.red;
             transform.parent.GetComponent<SpriteRenderer>().color = spriteRenderer.color; //TEST: to see easily cows.
-		}
-	}
-	#endregion
+        }
+    }
+    #endregion
 
-	void Awake()
-	{
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		player = FindObjectOfType<Die>();
-	}
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        player = FindObjectOfType<Die>();
+    }
 
-	protected override void Start()
-	{
+    protected override void Start()
+    {
         base.Start(); //Notificable will self-subscribe.
-        if(randomSide)
-            SelectRandomSide();
-	}
+        SelectRandomSide();
+    }
 
     /// <summary>
     /// Chooses a random side if applicable — it is, <see cref="randomSide"/>.
     /// </summary>
 	void SelectRandomSide()
-	{
+    {
         if(!randomSide)
             return;
 
-		sideRequired = Random.Range(1, 7); //'max' exclusive when int randomization.
-		spriteRenderer.sprite = sprites[sideRequired - 1];
-	}
+        sideRequired = Random.Range(1, 7); //'max' exclusive when int randomization.
+        spriteRenderer.sprite = sprites[sideRequired - 1];
+    }
 
-	void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.tag == "Player")
-			CheckSuccess();
-	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+            CheckSuccess();
+    }
 
     #region Notifications
     /// <summary>
@@ -98,92 +97,92 @@ public class Mark : Notificable
     /// <para><see cref="Notification.Dead"/>: reactivation.</para>
     /// </summary>
     protected override void ConfigureSubscriptions()
-	{
-		subscriptions = Notification.Beep | Notification.Flip | Notification.Dead;
-	}
+    {
+        subscriptions = Notification.Beep | Notification.Flip | Notification.Dead;
+    }
 
     /// <summary>
     /// Checking state every beep, towards state changes if necessary.
     /// </summary>
     public override void OnBeep()
-	{
-		CheckRight();
-	}
+    {
+        CheckRight();
+    }
 
-	/// <summary>
-	/// Checking state when die changes side, towards skin changes if necessary.
-	/// </summary>
+    /// <summary>
+    /// Checking state when die changes side, towards skin changes if necessary.
+    /// </summary>
     /// <remarks>
     /// Just for better visual response. If player flips die side early, there is
     /// a little offset until next beep, so mark feels taking a long time to set.
     /// </remarks>
-	public override void OnFlip()
-	{
-		CheckRight();
-	}
+    public override void OnFlip()
+    {
+        CheckRight();
+    }
 
     /// <summary>
     /// Reactivate if was passed, and new side. So far, random selection.
     /// </summary>
 	public override void OnDead()
-	{
-		SelectRandomSide();
-		spriteRenderer.enabled = true;
-	}
-	#endregion
+    {
+        SelectRandomSide();
+        spriteRenderer.enabled = true;
+    }
+    #endregion
 
-	protected virtual void CheckRight()
-	{
-		if (IsRight())
-			OnRight();
-		else
-			OnWrong();
-	}
+    protected virtual void CheckRight()
+    {
+        if(IsRight())
+            OnRight();
+        else
+            OnWrong();
+    }
 
-	protected bool IsRight()
-	{
-		return player.Side == sideRequired;
-	}
+    protected bool IsRight()
+    {
+        return player.Side == sideRequired;
+    }
 
-	protected virtual void OnRight()
-	{
-		State = true;
-	}
+    protected virtual void OnRight()
+    {
+        State = true;
+    }
 
-	protected virtual void OnWrong()
-	{
-		State = false;
-	}
+    protected virtual void OnWrong()
+    {
+        State = false;
+    }
 
-	/// <summary>
-	/// Actual checking of mark state. Usually happens when die reaches mark trigger.
-	/// </summary>
-	public void CheckSuccess()
-	{
-		if (State)
-			OnSuccess();
-		else
-			OnFailure();
-	}
+    /// <summary>
+    /// Actual checking of mark state. Usually happens when die reaches mark trigger.
+    /// </summary>
+    public void CheckSuccess()
+    {
+        if(State)
+            OnSuccess();
+        else
+            OnFailure();
+    }
 
-	/// <summary>
-	/// Obstacle passed.
-	/// </summary>
-	protected virtual void OnSuccess()
-	{
-		//TODO: replay by effect on gameplay.
-		Debug.Log("Success");
-		//Listening = false;
-		spriteRenderer.enabled = false;
-	}
+    /// <summary>
+    /// Obstacle passed.
+    /// </summary>
+    protected virtual void OnSuccess()
+    {
+        //TODO: replay by effect on gameplay.
+        Debug.Log("Success");
+        //Listening = false;
+        spriteRenderer.enabled = false;
+    }
 
-	/// <summary>
-	/// Obstacle not passed.
-	/// </summary>
-	protected virtual void OnFailure()
-	{
-		//TODO: replace by effect on gameplay. Death, rewind, -1 lives... whatever.
-		Debug.Log("Fail");
-		notifier.NotificateFail();
-	}
+    /// <summary>
+    /// Obstacle not passed.
+    /// </summary>
+    protected virtual void OnFailure()
+    {
+        //TODO: replace by effect on gameplay. Death, rewind, -1 lives... whatever.
+        Debug.Log("Fail");
+        notifier.NotificateFail();
+    }
 }
